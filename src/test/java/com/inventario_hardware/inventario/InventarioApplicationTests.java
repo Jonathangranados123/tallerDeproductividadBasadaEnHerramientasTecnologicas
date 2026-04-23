c2006a817bfc8643012320280c1275f3ddf5a8af
Jonathangranados123 <123481552+Jonathangranados123@users.noreply.github.com>
2026-04-22 22:06:37 -0600

    travis pero con master

--- src/test/java/com/inventario_hardware/inventario/InventarioApplicationTests.java
+++ src/test/java/com/inventario_hardware/inventario/InventarioApplicationTests.java
@@ -3,11 +3,15 @@ package com.inventario_hardware.inventario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class InventarioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+		@Test
+		void buildRuns() {
+			assertTrue(true);
+
+		}
 }
